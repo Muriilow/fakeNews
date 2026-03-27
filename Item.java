f8fb01ac22f0efef15e0e2dc6db7c7177a2416e9
Vinileonn <vinicius.leon@ufpr.com>
2026-03-27 18:35:35 -0300

    arrumando item

--- Item.java
+++ Item.java
@@ -1,20 +1,21 @@
 public class Item {
-    private Type type;
+    private Type type[];
 
     public Item(Type type)
     {
         this.SetType(type);
     }
 
-    public Type GetType()
+    public Type GetType(int index)
     {
-        return type;
+        return type[index];
     }
 
     public void SetType(Type type)
     {
         if(type == null)
             return;
+        //arrumar para vetor
         this.type = type;
     }
 }
